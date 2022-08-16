521e5be3531f33d9776b475bf3278cb5a9e5111f
Lígia Bicalho <ligiabicalho@gmail.com>
2022-08-16 09:58:49 -0300

    Correção Requisito 20

--- script.js
+++ script.js
@@ -23,72 +23,74 @@ checkBox.addEventListener('change', habilitar);
 
 function limitTextArea(valor) {
   const quant = 500;
-  const total = valor.length;
+  const total = valor.target.textLength;
   if (total <= quant) {
     const resto = quant - total;
     document.getElementById('counter').innerHTML = resto;
   }
 }
+const textArea = document.getElementById('textarea');
+textArea.addEventListener('keyup', limitTextArea);
 
-const nome = document.querySelector('#input-name');
-const sobrenome = document.querySelector('#input-lastname');
-const email = document.querySelector('#input-email');
-const casa = document.querySelector('#house');
-const familia = document.querySelector('#label-family');
-const conteudo = document.querySelector('#label-content');
-const avaliacao = document.querySelector('#label-rate');
-const observacoes = document.querySelector('#textarea');
-const forms = document.querySelector('#form-data');
+// const nome = document.querySelector('#input-name');
+// const sobrenome = document.querySelector('#input-lastname');
+// const email = document.querySelector('#input-email');
+// const casa = document.querySelector('#house');
+// const familia = document.querySelector('#label-family');
+// const conteudo = document.querySelector('#label-content');
+// const avaliacao = document.querySelector('#label-rate');
+// const observacoes = document.querySelector('#textarea');
+// const forms = document.querySelector('#form-data');
 
-function getCasa() {
-  return casa.options[casa.selectedIndex].value;
-}
+// function getCasa() {
+//   return casa.options[casa.selectedIndex].value;
+// }
 
-function getFamilia () {
-  for (let i = 0; i < familia.length; i += 1) {
-    if (familia[i].checked) {
-      return familia[i].value;
-    }
-  }
-}
+// function getFamilia () {
+//   for (let i = 0; i < familia.length; i += 1) {
+//     if (familia[i].checked) {
+//       return familia[i].value;
+//     }
+//   }
+// }
 
-function getConteudo () {
-  for (let i = 0; i < conteudo.length; i += 1) {
-    if (conteudo[i].checked) {
-      return conteudo[i].value;
-    }
-  }
-}
+// function getConteudo () {
+//   for (let i = 0; i < conteudo.length; i += 1) {
+//     if (conteudo[i].checked) {
+//       return conteudo[i].value;
+//     }
+//   }
+// }
 
-function getAvaliacao() {
-  return avaliacao.options[avaliacao.selectedIndex].value;
-}
+// function getAvaliacao() {
+//   return avaliacao.options[avaliacao.selectedIndex].value;
+// }
 
-const pNomeSobrenome = document.createElement('p');
-const pEmail = document.createElement('p');
-const pCasa = document.createElement('p');
-const pFamilia = document.createElement('p');
-const pConteudo = document.createElement('p');
-const pAvaliacao = document.createElement('p');
-const pObservacoes = document.createElement('p');
+// const pNomeSobrenome = document.createElement('p');
+// const pEmail = document.createElement('p');
+// const pCasa = document.createElement('p');
+// const pFamilia = document.createElement('p');
+// const pConteudo = document.createElement('p');
+// const pAvaliacao = document.createElement('p');
+// const pObservacoes = document.createElement('p');
 
-function addItemForms (e) {
-  e.preventDefault();
-  pNomeSobrenome.innerText = 'Nome: ' + nome.value + sobrenome.value;
-  pEmail.innerText = 'Email: ' + email.value;
-  pCasa.innerText = 'Casa :' + getCasa();
-  pFamilia.innerText = 'Familia: ' + getFamilia();
-  pConteudo.innerText = 'Matérias: ' + getConteudo();
-  pAvaliacao.innerText = 'Avaliação: ' + getAvaliacao();
-  pObservacoes.innerText = 'Observações: ' + observacoes.value;
-  forms.appendChild(pNomeSobrenome);
-  forms.appendChild(pEmail);
-  forms.appendChild(pCasa);
-  forms.appendChild(pFamilia);
-  forms.appendChild(pConteudo);
-  forms.appendChild(pAvaliacao);
-  forms.appendChild(pObservacoes);
-  console.log('Oi');
-}
+// function addItemForms (e) {
+//   e.preventDefault();
+//   pNomeSobrenome.innerText = 'Nome: ' + nome.value + sobrenome.value;
+//   pEmail.innerText = 'Email: ' + email.value;
+//   pCasa.innerText = 'Casa :' + getCasa();
+//   pFamilia.innerText = 'Familia: ' + getFamilia();
+//   pConteudo.innerText = 'Matérias: ' + getConteudo();
+//   pAvaliacao.innerText = 'Avaliação: ' + getAvaliacao();
+//   pObservacoes.innerText = 'Observações: ' + observacoes.value;
+//   forms.appendChild(pNomeSobrenome);
+//   forms.appendChild(pEmail);
+//   forms.appendChild(pCasa);
+//   forms.appendChild(pFamilia);
+//   forms.appendChild(pConteudo);
+//   forms.appendChild(pAvaliacao);
+//   forms.appendChild(pObservacoes);
+//   console.log('Oi');
+// }
 
-bntEnviar.addEventListener('click', addItemForms);
+// bntEnviar.addEventListener('click', addItemForms);
